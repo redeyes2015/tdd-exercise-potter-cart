df745f53f0e534d935eb159f198f2996fad0a26f
Ryan.Chang <ryan.chang@vivotek.com>
2018-02-07 19:34:53 +0800

    Add first test scenario: one book

---
+++ test/cart_test.js
@@ -0,0 +1,35 @@
+/*
+Feature: PotterShoppingCart
+        In order to 提供最便宜的價格給來買書的爸爸媽媽
+        As a 佛心的出版社老闆
+        I want to 設計一個哈利波特的購物車
+*/
+const PotterCart = require("../potter_cart.js");
+const t = require('tap');
+
+/*
+Scenario: 第一集買了一本，其他都沒買，價格應為100*1=100元
+        Given 第一集買了 1 本
+        And 第二集買了 0 本
+        And 第三集買了 0 本
+        And 第四集買了 0 本
+        And 第五集買了 0 本
+        When 結帳
+        Then 價格應為 100 元
+*/
+t.test('buying_1_first_should_be_100_dollor', (t) => {
+  //arrange
+  const cart = new PotterCart();
+  cart.add(PotterCart.First, 1);
+  const expected = 100;
+
+  //act
+  const actual = cart.getPrice();
+
+  //assert
+  t.equal(actual, expected);
+
+  t.end();
+});
+
+
