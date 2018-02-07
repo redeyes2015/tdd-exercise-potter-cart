130b8b1e7db54429fa4b67bc70c050249018a35f
Ryan.Chang <ryan.chang@vivotek.com>
2018-02-07 19:50:30 +0800

    First PotterCart implementation

---
+++ potter_cart.js
@@ -0,0 +1,42 @@
+const First = Symbol();
+const Second = Symbol();
+const Third = Symbol();
+const Fourth = Symbol();
+const Fifth = Symbol();
+
+const AllBooks = [
+  First,
+  Second,
+  Third,
+  Fourth,
+  Fifth,
+];
+
+class PotterCart {
+  constructor () {
+    this[First] = 0;
+    this[Second] = 0;
+    this[Third] = 0;
+    this[Fourth] = 0;
+    this[Fifth] = 0;
+  }
+  add(book, count) {
+    if (!book) {
+      throw new Error('unknown book');
+    }
+    this[book] += count;
+  }
+  getPrice() {
+    return AllBooks.reduce((acc, book) => {
+      return acc += this[book] * 100;
+    }, 0);
+  }
+}
+
+PotterCart.First = First;
+PotterCart.Second = Second;
+PotterCart.Third = Third;
+PotterCart.Fourth = Fourth;
+PotterCart.Fifth = Fifth;
+
+module.exports = PotterCart;
